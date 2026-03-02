98418fd70bf09de8514eb27f10a9652789c6702d
AndresMarolt <andresmmarolt@gmail.com>
2026-03-02 18:47:28 +0100

    fix: resolve ESLint errors in redux reducers 2

--- src/redux/reducers/tenants.js
+++ src/redux/reducers/tenants.js
@@ -1,24 +1,40 @@
-import { START_LOADING, FINISH_LOADING, FETCH_TENANTS, FETCH_ONE, CREATE_TENANT, UPDATE_TENANT, DELETE_TENANT } from "../../constants/actionTypes"
+import {
+  START_LOADING,
+  FINISH_LOADING,
+  FETCH_TENANTS,
+  FETCH_ONE,
+  CREATE_TENANT,
+  UPDATE_TENANT,
+  DELETE_TENANT,
+} from "../../constants/actionTypes";
 
-const tenantsReducer = (state = {isLoading: true, tenants: []}, action) => {
-    switch(action.type) {
-        case START_LOADING:
-            return {...state, isLoading: true}
-        case FINISH_LOADING:
-            return {...state, isLoading: false}
-        case FETCH_TENANTS:
-            return {...state, tenants: action.payload}
-        case FETCH_ONE:
-            return {...state, tenant: action.payload}
-        case CREATE_TENANT:
-            return {...state, tenants: [...state.tenants, action.payload]};
-        case UPDATE_TENANT:
-            return {...state, tenants: state.tenants.map(ten => (ten._id === action.payload._id ? action.payload : ten))}
-        case DELETE_TENANT:
-            return {...state, tenants: state.tenants.filter(ten => ten._id !== action.payload)};
-        default:
-            return state;
-    }
-}
+const tenantsReducer = (state = { isLoading: true, tenants: [] }, action) => {
+  switch (action.type) {
+    case START_LOADING:
+      return { ...state, isLoading: true };
+    case FINISH_LOADING:
+      return { ...state, isLoading: false };
+    case FETCH_TENANTS:
+      return { ...state, tenants: action.payload };
+    case FETCH_ONE:
+      return { ...state, tenant: action.payload };
+    case CREATE_TENANT:
+      return { ...state, tenants: [...state.tenants, action.payload] };
+    case UPDATE_TENANT:
+      return {
+        ...state,
+        tenants: state.tenants.map((ten) =>
+          ten._id === action.payload._id ? action.payload : ten,
+        ),
+      };
+    case DELETE_TENANT:
+      return {
+        ...state,
+        tenants: state.tenants.filter((ten) => ten._id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
 
 export default tenantsReducer;
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,21 +1,24 @@
 // unused action types removed since they aren't referenced here
 
-const userReducer = (state = {authData: null, error: null, isLoading: true}, action) => {
-    switch(action.type) {
-        case 'AUTH':
-            localStorage.setItem('token', action.apiRes.data.token);
-            return {...state, authData: action?.apiRes.data, error: null};
-        case 'LOGOUT':
-            return {...state, authData: null, error: null}
-        case 'ERROR':
-            return {...state, authData: null, error: action.error}
-        case 'START_LOADING':
-            return {...state, isLoading: true}
-        case 'FINISH_LOADING':
-            return {...state, isLoading: false}
-        default:
-            return state;
-    }
-}
+const userReducer = (
+  state = { authData: null, error: null, isLoading: true },
+  action,
+) => {
+  switch (action.type) {
+    case "AUTH":
+      localStorage.setItem("token", action.apiRes.data.token);
+      return { ...state, authData: action?.apiRes.data, error: null };
+    case "LOGOUT":
+      return { ...state, authData: null, error: null };
+    case "ERROR":
+      return { ...state, authData: null, error: action.error };
+    case "START_LOADING":
+      return { ...state, isLoading: true };
+    case "FINISH_LOADING":
+      return { ...state, isLoading: false };
+    default:
+      return state;
+  }
+};
 
 export default userReducer;

256c755472885dc3c853eea990454ee2f09e4039
AndresMarolt <andresmmarolt@gmail.com>
2026-03-02 18:55:49 +0100

    Fix lint errors

--- src/components/Admin/Pages/LandlordsPage/LandlordsPage.js
+++ src/components/Admin/Pages/LandlordsPage/LandlordsPage.js
@@ -1,60 +1,78 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import TenantsTable from "./Table/Table";
 import { getLandlord, getLandlords } from "../../../../redux/actions/landlords";
 import Modal from "./Modal/Modal";
-import { useDispatch} from 'react-redux'
+import { useDispatch } from "react-redux";
 
-const TenantsPage = () => {
+const LandlordsPage = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const fetchLandlords = async () => {
+    dispatch(getLandlords());
+  };
 
-    const fetchLandlords = async () => {
-        dispatch(getLandlords());
-    }
+  const [showModal, setShowModal] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-    const [showModal, setShowModal] = useState(false);
-    const [isEditing, setIsEditing] = useState(false);
+  const onEdit = async (id) => {
+    setIsEditing(true);
+    await dispatch(getLandlord(id));
+    setShowModal(true);
+  };
 
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Datos Personales",
+        columns: [
+          {
+            Header: "Nombre Completo",
+            accessor: "fullName",
+          },
+          {
+            Header: "Domicilio",
+            accessor: "address",
+          },
+          {
+            Header: "Localidad",
+            accessor: "city",
+          },
+        ],
+      },
+    ],
+    [],
+  );
 
-    const onEdit = async (id) => {
-        setIsEditing(true);
-        await dispatch(getLandlord(id))
-        setShowModal(true);
-    }
-
-    const columns = useMemo(() => [
-        {
-            Header: 'Datos Personales',
-            columns: [
-                {
-                    Header: 'Nombre Completo',
-                    accessor: 'fullName'
-                },
-                {
-                    Header: 'Domicilio',
-                    accessor: 'address'
-                },
-                {
-                    Header: 'Localidad',
-                    accessor: 'city'
-                }
-            ]
-        }
-    ])
-
+  useEffect(() => {
     fetchLandlords();
+  }, []);
 
-    return (
-        <>
-            <button className="mb-4 btn btn-success" onClick={() => {setIsEditing(false); setShowModal(true)}}>Agregar un nuevo propietario</button>
+  return (
+    <>
+      <button
+        className="mb-4 btn btn-success"
+        onClick={() => {
+          setIsEditing(false);
+          setShowModal(true);
+        }}
+      >
+        Agregar un nuevo propietario
+      </button>
 
-            {
-                showModal && <Modal setShowModal={setShowModal} mode={isEditing ? 'Editar' : 'Agregar'}/>
-            }
+      {showModal && (
+        <Modal
+          setShowModal={setShowModal}
+          mode={isEditing ? "Editar" : "Agregar"}
+        />
+      )}
 
-            <TenantsTable columns={columns} onEdit={onEdit} setIsEditing={setIsEditing} />
-        </>
-    )
-}
+      <TenantsTable
+        columns={columns}
+        onEdit={onEdit}
+        setIsEditing={setIsEditing}
+      />
+    </>
+  );
+};
 
-export default TenantsPage;
+export default LandlordsPage;
